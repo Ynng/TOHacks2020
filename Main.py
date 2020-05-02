75619f0ca7ca350993841129f5db8375cdd439b4
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-02 17:13:36 -0400

    rando stuff

--- Main.py
+++ Main.py
@@ -3,12 +3,7 @@ from absl import logging
 import tensorflow as tf
 
 import tensorflow_hub as hub
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-import pandas as pd
-import re
-import seaborn as sns
 
 module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 model = hub.load(module_url)
@@ -18,9 +13,10 @@ def similarity(message1, message2):
   message_embeddings_ = model([message1,message2])
   return np.inner(message_embeddings_, message_embeddings_)[0][1]
 
-print(similarity("programmer", "learn python in one hour"))
-print(similarity("anime", "weeb"))
-print(similarity("school", "beating minecraft with a steering wheel"))
+while True:
+  msg1 = input("Enter String 1:")
+  msg2 = input("Enter String 2:")
+  print("Similarity: ", similarity(msg1, msg2))
 
 
 
