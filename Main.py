07bce0c1e9b69bf27461b5317daba5b0f6239ec8
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-02 19:57:13 -0400

    finished python web server

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
 from absl import logging
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import time
 import tensorflow as tf
 
 import tensorflow_hub as hub
 import numpy as np
+
+hostName = "localhost"
+hostPort = 9000
+
 
 module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 model = hub.load(module_url)
@@ -13,11 +20,36 @@ def similarity(message1, message2):
   message_embeddings_ = model([message1,message2])
   return np.inner(message_embeddings_, message_embeddings_)[0][1]
 
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        title = query_components["title"].replace("%20", " ")
+        print("title = ", title)
+
+        # query_components = { "imsi" : "Hello" }
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes("{\"similarity\":", "utf-8"))
+        self.wfile.write(bytes(str(similarity("programming", title)), "utf-8"))
+        self.wfile.write(bytes("}", "utf-8"))
+
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+
+
 while True:
   msg1 = input("Enter String 1:")
   msg2 = input("Enter String 2:")
   print("Similarity: ", similarity(msg1, msg2))
-
-
-
-
