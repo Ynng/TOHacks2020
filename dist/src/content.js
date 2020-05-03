b59ee8d078c39b769ee0007eef73f000435d9fe5
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-02 22:32:57 -0400

    simple chrome extension

--- dist/src/content.js
+++ dist/src/content.js
@@ -1,3 +1,4 @@
 const HIGH_CONFIDENCE_THRESHOLD = 0.5;
 const LOW_CONFIDENCE_THRESHOLD = 0.1;
 
+console.log(window.location.href)
--- src/background.js
+++
@@ -1,6 +0,0 @@
-console.log("It's working!")
-
-chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-  console.log("Ayyeeee");
-  console.log(tabs[0]);
-});
